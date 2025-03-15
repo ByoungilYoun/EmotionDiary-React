548dbcc56587462bffe24f4d0c4975fc53b3515c
ByoungilYoun <jimmy7525@naver.com>
2025-03-16 00:23:39 +0900

    이미지, 폰트 적용하기

--- EmotionDiary-React/EmotionDiary/src/App.tsx
+++ EmotionDiary-React/EmotionDiary/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.tsx";
 import New from "./pages/New.tsx";
 import Diary from "./pages/Diary.tsx";
 import Notfound from "./pages/Notfound.tsx";
+import { getEmotionImage } from "./util/get-emotion-image.ts";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -21,6 +22,14 @@ function App() {
 
     return (
         <>
+            <div>
+                {/* assets 폴더에 이미지를 넣었을 경우 import 하고 이미지를 가져옴, data uri 형태로 메모리 캐싱됨, 따라서 assets 에 넣고 사용하면 된다. */}
+                <img src={getEmotionImage(1) || ""}/>
+                <img src={getEmotionImage(2) || ""}/>
+                <img src={getEmotionImage(3) || ""}/>
+                <img src={getEmotionImage(4) || ""}/>
+                <img src={getEmotionImage(5) || ""}/>
+            </div>
             <div>
                 <Link to={"/"}>Home</Link>
                 <Link to={"/new"}>New</Link>
--- EmotionDiary-React/EmotionDiary/src/pages/Home.tsx
+++ EmotionDiary-React/EmotionDiary/src/pages/Home.tsx
@@ -1,9 +1,5 @@
-import { useSearchParams } from "react-router-dom"; // query parameter 를 가져오는 hook
 
 const Home = () => {
-    const [params, setParams] = useSearchParams();
-    console.log(params.get("value"));
-
     return (
         <div>Home</div>
     )
