56539e9f89b0505743a2f8208a2b11bcdab93fac
ByoungilYoun <jimmy7525@naver.com>
2025-03-15 23:45:47 +0900

    페이지 라우팅 동적 경로 설정

--- EmotionDiary-React/EmotionDiary/src/pages/Diary.tsx
+++ EmotionDiary-React/EmotionDiary/src/pages/Diary.tsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom"; // url parameter 값을 가져오는 hook
+
 const Diary = () => {
+    const params = useParams();
+    console.log(params);
+
     return (
-        <div>Diary</div>
+        <div>{params.id} 번 일기입니다.</div>
     )
 }
 
--- EmotionDiary-React/EmotionDiary/src/pages/Home.tsx
+++ EmotionDiary-React/EmotionDiary/src/pages/Home.tsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom"; // query parameter 를 가져오는 hook
+
 const Home = () => {
+    const [params, setParams] = useSearchParams();
+    console.log(params.get("value"));
+
     return (
         <div>Home</div>
     )
